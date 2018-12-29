5e7ffd05543d3d6ecbca3f689b9d5e9dacbeb68e
b.palowski <brianpalowski00@gmail.com>
2018-12-29 14:22:11 -0800

    Upated UI

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 //listen for a submit being sent
-document
-  .querySelector("#loan-form")
-  .addEventListener("submit", calculateResults);
+document.querySelector("#loan-form").addEventListener("submit", function(e) {
+  //show loader
+  document.getElementById("loading").style.display = "block";
+
+  setTimeout(calculateResults, 1000);
+
+  e.preventDefault();
+});
 
 //Calculate results
-function calculateResults(e) {
+function calculateResults() {
   console.log("Calculating...");
   // UI Vars
   const amount = document.getElementById("amount");
@@ -26,14 +31,20 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+    //show results
+    document.getElementById("results").style.display = "block";
+    //hide loader
+    document.getElementById("loading").style.display = "none";
   } else {
     showError("Please check your numbers");
   }
-
-  e.preventDefault();
 }
 // Show Error Function
 function showError(error) {
+  //hide results
+  document.getElementById("results").style.display = "none";
+  //hide loader
+  document.getElementById("loading").style.display = "none";
   const errorDiv = document.createElement("div");
 
   // get elements
